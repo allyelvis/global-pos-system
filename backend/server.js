129c5fdb8fb2d20a1f8b963bef7b81c62540a601
allyelvis <itsupport@aenzbi.bi>
2024-10-05 21:56:45 +0000

    all

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Product = require('./database/pos_schema').Product;
+const Order = require('./database/pos_schema').Order;
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
+
+app.post('/addProduct', async (req, res) => {
+  const product = new Product(req.body);
+  try {
+    await product.save();
+    res.status(201).send(product);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get('/getProducts', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/createOrder', async (req, res) => {
+  const order = new Order(req.body);
+  try {
+    await order.save();
+    res.status(201).send(order);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
